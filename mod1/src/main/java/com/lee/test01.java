6e498b22674095d7f6512357d6b78f872dc33ca1
Administrator <13664419283@139.com>
2022-04-21 16:31:39 +0800

    init2xxxxxxxxxxxxxxxxxx

---
+++ mod1/src/main/java/com/lee/test01.java
@@ -0,0 +1,18 @@
+package com.lee;
+
+/**
+ * @projectName: protest
+ * @package: com.lee
+ * @className: test01
+ * @author: liyw
+ * @description: TODO
+ * @date: 2022/4/21 15:58
+ * @version: 1.0
+ */
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println("xxx");
+        System.out.println("777");
+        System.out.println("777");
+    }
+}
