6805aa59dac8aef372f73c758b8fabfac126a8ac
Administrator <13664419283@139.com>
2022-04-21 16:39:10 +0800

    init2xxxxxxxxxxxxxxxxxx

---
+++ mod1/src/main/java/com/lee/tes02.java
@@ -0,0 +1,13 @@
+package com.lee;
+
+/**
+ * @projectName: protest
+ * @package: com.lee
+ * @className: tes02
+ * @author: liyw
+ * @description: TODO
+ * @date: 2022/4/21 15:59
+ * @version: 1.0
+ */
+public class tes02 {
+}
